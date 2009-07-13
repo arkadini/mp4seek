7ba8915b7c52f6b4ad547bcdee5538f5fc31411f
Jan Urbański <wulczer@wulczer.org>
2009-07-13 18:52:12 +0200

    Distutilize.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name="mp4seek",
+      version="1.0",
+      description="Seeking inside MP4 files",
+      platforms=["any"],
+      license="LGPL",
+      author="Arek Korbik",
+      maintainer="Arek Korbik",
+      author_email="arkadini@gmail.com",
+      maintainer_email="arkadini@gmail.com",
+      packages=["mp4seek"])
