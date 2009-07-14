eb725cfced126bc41341b7938789e9eb37a1808e
Jan Urbański <wulczer@wulczer.org>
2009-07-14 10:27:51 +0200

    Change license to MIT, add docstring.

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
+"""mp4seek
+=======
+
+A library for seeking inside MP4 files. Contains utilities to
+split ISO MPEG-4 files and rebuild the necessary headers for
+the output to be compliant with the MP4 specification.
+"""
 
 from distutils.core import setup
 
 setup(name="mp4seek",
       version="1.0",
       description="Seeking inside MP4 files",
+      long_description=__doc__,
       platforms=["any"],
-      license="LGPL",
+      license="MIT",
       author="Arek Korbik",
       maintainer="Arek Korbik",
       author_email="arkadini@gmail.com",
